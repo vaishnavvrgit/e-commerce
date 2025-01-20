b01d63b28e9ee14e34fda87632026825c14a5358
vaishnavvrgit <vaishnavvr416@gmail.com>
2025-01-21 01:11:04 +0530

    header footer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,2 +1,116 @@
 @extends('layout.auth')
 
+@section('content')
+
+      <section class="signin-section">
+        <div class="container-fluid">
+          <!-- ========== title-wrapper start ========== -->
+          <div class="title-wrapper pt-30">
+            <div class="row align-items-center">
+              <div class="col-md-6">
+                <div class="title">
+                  <h2>Login</h2>
+                </div>
+              </div>
+           
+            </div>
+            <!-- end row -->
+          </div>
+          <!-- ========== title-wrapper end ========== -->
+
+          <div class="row g-0 auth-row">
+            <div class="col-lg-6">
+              <div class="auth-cover-wrapper bg-primary-100">
+                <div class="auth-cover">
+                  <div class="title text-center">
+                    <h1 class="text-primary mb-10">Welcome Back</h1>
+                    <p class="text-medium">
+                      login to your Existing account to continue
+                    </p>
+                  </div>
+                  <div class="cover-image">
+                    <img src="{{ asset('assets/images/auth/signin-image.svg') }}" alt="" />
+                  </div>
+                  <div class="shape-image">
+                    <img src="{{ asset('assets/images/auth/shape.svg') }}" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- end col -->
+            <div class="col-lg-6">
+              <div class="signin-wrapper">
+                <div class="form-wrapper">
+                  <h6 class="mb-15">Login here</h6>
+
+                  {{-- <p class="text-sm mb-25">
+                    Start creating the best possible user experience for you
+                    customers.
+                  </p> --}}
+
+                  <form action="{{ route('login_submit') }}" method="post">
+                    @csrf
+                    <div class="row">
+                      <div class="col-12">
+                        <div class="input-style-1">
+                          <label>Email</label>
+                          <input type="text" name="email" placeholder="Email" />
+                            @error('email')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                      </div>
+                      <!-- end col -->
+                      <div class="col-12">
+                        <div class="input-style-1">
+                          <label>Password</label>
+                          <input type="password" name="password" placeholder="Password" />
+                            @error('password')
+                                <div class="alert alert-danger">{{ $message }}</div>
+                            @enderror
+                        </div>
+                      </div>
+                      <!-- end col -->
+                      <div class="col-xxl-6 col-lg-12 col-md-6">
+                        <div class="form-check checkbox-style mb-30">
+                          <input class="form-check-input" type="checkbox" value="" id="checkbox-remember" />
+                          <label class="form-check-label" for="checkbox-remember">
+                            Remember me next time</label>
+                        </div>
+                      </div>
+                      <!-- end col -->
+                      <div class="col-xxl-6 col-lg-12 col-md-6">
+                        <div class="text-start text-md-end text-lg-start text-xxl-end mb-30">
+                          <a href="reset-password.html" class="hover-underline">
+                            Forgot Password?
+                          </a>
+                        </div>
+                      </div>
+                      <!-- end col -->
+                      <div class="col-12">
+                        <div class="button-group d-flex justify-content-center flex-wrap">
+                          <button class="main-btn primary-btn btn-hover w-100 text-center">
+                            Login
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- end row -->
+                  </form>
+                  <div class="singin-option pt-40">
+                
+                    <p class="text-sm text-medium text-dark text-center">
+                      Don’t have any account yet?
+                      <a href="signup.html">Create an account</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+         
+          </div>
+  
+        </div>
+      </section>
+
+@endsection
