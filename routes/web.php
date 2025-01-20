003183d2c1a59966fe77b127443c483611c88048
vaishnavvrgit <vaishnavvr416@gmail.com>
2025-01-21 00:33:12 +0530

    update code

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,28 @@
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/rollback', function () {
+    Artisan::call('migrate:rollback');
+    dd(Artisan::output());
+});
+
+
+Route::get('/migrate', function () {
+    Artisan::call('migrate');
+    dd(Artisan::output());
+});
+Route::get('/fresh', function () {
+    Artisan::call('migrate:fresh');
+    dd(Artisan::output());
+});
+
+
+Route::get('/db-seed', function () {
+    Artisan::call('db:seed');
+    dd(Artisan::output());
+});
+/*****************developer ******************************* */
+
 Route::get('/', function () {
     return view('welcome');
 });
